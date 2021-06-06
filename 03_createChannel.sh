bec08929046425bcf82c520f7659d67f50098c14
Manpreet Singh <manpreetsinghpuri01@gmail.com>
2021-06-06 20:09:21 +0530

    Trying to resolve errors, but unsuccessful

--- 03_createChannel.sh
+++ 03_createChannel.sh
@@ -1,4 +1,4 @@
-echo "====================================== Creating mychannel ========================================="
+echo "====================================== Creating mychannel (through cli, ignore for api)========================================="
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" -e "CORE_PEER_ADDRESS=peer0.org1.example.com:7051" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt" cli peer channel create -o orderer.example.com:7050 -c mychannel -f ./channel-artifacts/channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
 echo "=================================== Joining peer0.org1 to mychannel =================================="
--- 04_startNetwork.sh
+++ 04_startNetwork.sh
@@ -3,7 +3,7 @@
 ./01_generateCertificates.sh
 
 ./02_startContainers.sh
-
-./03_createChannel.sh
+# Only for cli commands, ignore in case of api
+./03_createChannel.sh       
 
 echo "=================================== Fabric Network is successfully up and running... ========================================="
