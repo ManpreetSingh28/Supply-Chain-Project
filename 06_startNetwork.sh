4876c7a5c286a2a2e08feb8f600f693f9e4d19af
Manpreet Singh <manpreetsinghpuri01@gmail.com>
2021-06-08 21:08:11 +0530

    Added a README file

--- 06_startNetwork.sh
+++
@@ -1,9 +0,0 @@
-./05_destroyNetwork.sh
-
-./01_generateCertificates.sh
-
-./02_startContainers.sh
-# Only for cli commands, ignore in case of api
-./03_createChannel.sh       
-
-echo "=================================== Fabric Network is successfully up and running... ========================================="
